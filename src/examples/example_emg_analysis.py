0fe9a43135c7dd63c4b85ab0c0043776b88bbf6d
Patrick Bings <patrick.bings@dfki.de>
2023-07-18 14:23:12 +0200

    changed the loading and processing process

--- src/examples/example_emg_analysis.py
+++ src/examples/example_emg_analysis.py
@@ -30,12 +30,12 @@ file_str = "21032023_AJ80D_curls_with_pause_set1.txt"
 
 # *** channel selection params (comment in for channel selection) ***
 #selected_channels =  []# ['\tR.Biceps Br.(uV)', '\tR.Ant.Deltoid(uV)', '\tR.Mid Delt.(uV)']
-#inverse = True
+inverse = False # define True for channel selection
 
 
 # for ANT Systems 
 emg_ch_names = ["EMG1", "EMG2", "EMG3", "EMG4", "EMG5", "EMG6", "EMG7", "EMG8"]
-#selected_channels = ["EMG2"]
+selected_channels = []
 
 
 # EMG sampling rate (Cometa default)
@@ -44,58 +44,23 @@ emg_ch_names = ["EMG1", "EMG2", "EMG3", "EMG4", "EMG5", "EMG6", "EMG7", "EMG8"]
 fsamp_emg = 500 # for ANT
 
 # *** processing parameters (if used comment in) *** 
-#target_frequency = 500 # target frequency after downsampling (Cometa)
+target_frequency = 500 # target frequency after downsampling (Cometa)
 
 # variance filter length 
-#n_var = 170 # calc length of variance filter (default 170, tested for 500 Hz sampling rate)
+n_var = 170 # calc length of variance filter (default 170, tested for 500 Hz sampling rate)
 
 # *********************************************************************************
-# ************************* Load EMG data  ****************************************
+# ************************* Load and process EMG data  ****************************
 # *********************************************************************************
 
-# creating EMG Object and load emg_data 
+# define the processing params
+ 
+load_data = 1 # 0 for Cometa, 1 vor ANT
+apply_var_filter = 0 # 0 -> no, 1 -> yes
+apply_bp_filter = 1 # 0 -> no, 1 -> yes
+plot_emg = 2 # 0 -> no plot, 1 -> plot raw data, 2 -> plot filtered data
 
-emg_data_obj = EMGData(1, file_str, fsamp_emg) #parse 0 for Cometa and 1 for ANT
+# create EMGData Object
 
-emg_data , emg_time_axis = emg_data_obj.get_raw_data()
-
-# loading emg data (Cometa)
-#file_str = os.path.join(data_path, file_str)
-#emg_data, emg_time_axis, emg_ch_names = EMGData.loadCometaEMGData(file_str)
-# (data, channels)
-
-# load EMG data ANT
-#emg_data, emg_time_axis = emg_data_obj.loadMiniANTEMGData(file_str, fsamp_emg)
-
-# *********************************************************************************
-# ************************* Process EMG data  *************************************
-# *********************************************************************************
-
-# *** Just comment in what should be used for processing *** 
-
-# channel selection, if inverse = False all channels specified are kept 
-#emg_data_select, emg_ch_names_select = EMGData.channelSelection(emg_data, emg_ch_names, selected_channels, inverse)
-
-
-# apply bandpass filter 
-emg_data_filtered = emg_data_obj.applyBPFilterRectifying(fsamp_emg, 20, 200, emg_data)
-
-# show all EMG channels
-emg_data_obj.showEMGData(emg_data, emg_time_axis, emg_ch_names)
-
-# show selected EMG channels
-#EMGData.showEMGData(emg_data_filtered, emg_time_axis, emg_ch_names_select)
-
-
-
-# downsampling to target frequency 
-#emg_data_down, time_axis_down = EMGData.decimateEMGData(emg_data_select, emg_time_axis, target_frequency, fsamp_emg)
-
-#EMGData.showEMGData(emg_data_down, time_axis_down, emg_ch_names_select)
-
-#apply variance filter to EMG signals  
-#emg_data_filtered = EMGData.applyVarianceFilter(emg_data_down, n_var)
-
-# show EMG signals in plots 
-#EMGData.showEMGData(emg_data_filtered, time_axis_down, emg_ch_names_select)
+emg_data_obj = EMGData(load_data, data_path, file_str, fsamp_emg, emg_ch_names, apply_var_filter, apply_bp_filter, plot_emg, selected_channels, inverse, target_frequency, n_var)
 
