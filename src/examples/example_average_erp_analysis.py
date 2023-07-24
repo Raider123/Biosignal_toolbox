363873132bc60f08ceb2737ea2968f4c6eab6d75
Patrick Bings <patrick.bings@dfki.de>
2023-07-24 14:36:44 +0200

    changed the loading and processing process

--- src/examples/example_average_erp_analysis.py
+++ src/examples/example_average_erp_analysis.py
@@ -94,39 +94,23 @@ for files_str in filenames:
     data_str_arr.append(os.path.join(data_path, files_str)) 
 data_str_arr = np.array(data_str_arr)
 
-#create EEGData object
-eeg_data_obj = EEGData(data_str_arr)
-
-#load EEG Data
-raw = eeg_data_obj.get_raw() # read data in brainproducts format
-
 
 # *********************************************************************************
 # **************** Make EEG average analysis **************************************
 # *********************************************************************************
 
+# select the EEGData params
+load_data = 1 # 0 -> No, 1 -> Yes
 
-# epoch the eeg data to trial length (for merged sets)
-erp_epochs, erp_epochs_obj, time_axis_eeg_epochs, remaining_eeg_channel_names, raw_filtered = eeg_data_obj.rereferencingEpoching(raw, marker_number, error_number, channel_list, inverse_keep_channel, reref_channel, apply_filter, f_highpass, f_lowpass, event_id_used, epoching_time_before_onset, epoching_time_after_onset, f_samp_eeg, apply_baseline_correction,  t0_baseline, t1_baseline)
-#erp_epochs has shape (trials, eeg-channels, sampels)
+#create EEGData object
+eeg_data_obj = EEGData(load_data, data_str_arr, marker_number, error_number, channel_list, inverse_keep_channel, reref_channel, apply_filter, f_highpass, f_lowpass, event_id_used, epoching_time_before_onset, epoching_time_after_onset, f_samp_eeg, apply_baseline_correction,  t0_baseline, t1_baseline, plot_montage, rename_channels, min_val, max_val, topoplot_times, topoplot_title_str, channel_to_evaluate)
 
-
-average_erp_epochs = np.mean(erp_epochs, axis = 0) # average the trials (average analysis), has now shape(channel, sampels) 
-
-# create an acticap montage 
-acticap_montage = eeg_data_obj.createActicapMontage(plot_montage, rename_channels)
-raw_filtered.set_montage(acticap_montage) # set created montage 
-
-# # make a topoplot 
-eeg_data_obj.topoplot(average_erp_epochs, time_axis_eeg_epochs, raw_filtered, topoplot_times, topoplot_title_str, min_val, max_val, f_samp_eeg)
-
-# show average erp signal selected channel 
-channel_index = remaining_eeg_channel_names.index(channel_to_evaluate)
-average_eeg_selected_channel = average_erp_epochs[channel_index, :]
+#load EEG Data
+raw = eeg_data_obj.get_raw() # read data in brainproducts format
 
 # Plot the selected channel (average)
 plt.figure()
-plt.plot(time_axis_eeg_epochs, average_eeg_selected_channel)
+plt.plot(eeg_data_obj.get_time_axis_eeg_epochs(), eeg_data_obj.get_average_eeg_selected_channel())
 plt.title("Average eeg signal for channel "+channel_to_evaluate)
 plt.xlabel("Time in seconds")
 plt.ylabel("Voltage in V")
