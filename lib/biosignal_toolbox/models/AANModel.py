c0c9343ff5d1d71594783ecbb2ef557d1c4e2a68
kschari <kartik.chari@dfki.de>
2024-08-26 18:07:37 +0200

    changed activation function from tanh to relu and linear

--- lib/biosignal_toolbox/models/AANModel.py
+++ lib/biosignal_toolbox/models/AANModel.py
@@ -9,8 +9,8 @@ def AAN_Model(layer1_neurons = 200, layer2_neurons = 15, feature_dim = 200):
     model = Sequential()
     
     # what is shape of: self.train_inp_r[1].__len__() ? 
-    model.add(Dense(layer1_neurons, input_dim = feature_dim, activation= 'tanh')) # 110 inputs in first layer, 7 neurons in hidden layer
-    model.add(Dense(layer2_neurons, activation = 'tanh')) #second hidden layer with x neurons /softplus, relu, linear
-    model.add(Dense(1, activation = 'tanh')) # 1 neuron in output layer
+    model.add(Dense(layer1_neurons, input_dim = feature_dim, activation= 'relu')) # 110 inputs in first layer, 7 neurons in hidden layer
+    model.add(Dense(layer2_neurons, activation = 'relu')) #second hidden layer with x neurons /softplus, relu, linear
+    model.add(Dense(1, activation = 'linear')) # 1 neuron in output layer
 
     return model 
