63115363d94e913c766107f96dc2db27312ad60f
kschari <kartik.chari@dfki.de>
2025-08-28 13:26:48 +0200

    ✨ Improved getAbsolutePath util
    now you can call this function from anywhere and it will be able to get proper project root

--- src/examples/example_qualisys_analysis.py
+++ src/examples/example_qualisys_analysis.py
@@ -1,11 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
-from biosignal_toolbox.utils import loadConfig, resolvePath, plotResults
+from biosignal_toolbox.utils import getAbsolutePath
 from biosignal_toolbox.motion_lib import MotionData
-
-#? get the path of the project root
-project_root = Path(__file__).resolve().parent.parent.parent
 
 #? create a qualisys motion data object
 qualisys_data = MotionData(data_path= "data/test/", filename='complex_0g_set6.tsv')
@@ -15,7 +12,7 @@ qualisys_data.calculateTorque()
 qualisys_data.saveTorques_npy(save_torques=True, save_dir="data/test", joint_to_save=['all'])
 
 #? plot front shoulder torque profile
-numpy_file_path = resolvePath("data/test/quali_torque_shoulder_front_complex_0g_set6.npy", project_root)
+numpy_file_path = getAbsolutePath("data/test/quali_torque_shoulder_front_complex_0g_set6.npy")
 data = np.load(numpy_file_path)
 plt.plot(qualisys_data.time_axis, data)
 plt.grid(linestyle=':', linewidth=1.5)
