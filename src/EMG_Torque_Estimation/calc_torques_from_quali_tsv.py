b2e29ec0c37ff7a733ad761dd9b620a9325f3aa7
kschari <kartik.chari@dfki.de>
2025-09-02 13:18:23 +0200

    🐛 Added yaml entry for subject weight

--- src/EMG_Torque_Estimation/calc_torques_from_quali_tsv.py
+++ src/EMG_Torque_Estimation/calc_torques_from_quali_tsv.py
@@ -46,7 +46,7 @@ for mov_idx, wgt_idx, set_idx in itertools.product(cfg.data_param.mov_type,
         #? create a qualisys motion data object
         qualisys_data = MotionData(data_path= cfg.filepath.data_path, filename=cfg.filepath.quali_tsv_path+file.name)
         #? calculate the torques from qualisys .tsv
-        qualisys_data.calculateTorque(body_weight_kg=80, obj_weight_g=int(re.search(r"\d+", wgt_idx).group()))
+        qualisys_data.calculateTorque(body_weight_kg=cfg.data_param.subject_weight, obj_weight_g=int(re.search(r"\d+", wgt_idx).group()))
         #? save the torques into individual .npy files
         qualisys_data.saveTorques_npy(save_torques=True, save_dir=cfg.filepath.quali_torques_save_path, joints_to_save=['all'])
 
