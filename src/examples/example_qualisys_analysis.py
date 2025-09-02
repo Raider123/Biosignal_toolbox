d2e360b0d97db64520fcd932917030f977a45083
AnasHo03 <anashomsi0317@gmail.com>
2025-09-02 12:52:05 +0200

    Added torque calculation and plotting script, changed channel names in motion_lib

--- src/examples/example_qualisys_analysis.py
+++ src/examples/example_qualisys_analysis.py
@@ -5,18 +5,18 @@ from biosignal_toolbox.utils import getAbsolutePath
 from biosignal_toolbox.motion_lib import MotionData
 
 #? create a qualisys motion data object
-qualisys_data = MotionData(data_path= "data/test/", filename='complex_0g_set6.tsv')
+qualisys_data = MotionData(data_path= "data/qualisys/BU62D/", filename='24_07_2025_BU62D_0g_grasp_1.tsv')
 #? calculate the torques from qualisys .tsv
 qualisys_data.calculateTorque()
 #? save the torques into individual .npy files
-qualisys_data.saveTorques_npy(save_torques=True, save_dir="data/test", joint_to_save=['all'])
+qualisys_data.saveTorques_npy(save_torques=True, save_dir="results/BU62D", joint_to_save=['all'])
 
 #? plot front shoulder torque profile
-numpy_file_path = getAbsolutePath("data/test/quali_torque_shoulder_front_complex_0g_set6.npy")
+numpy_file_path = getAbsolutePath("results/BU62D/quali_torque_elbow_24_07_2025_BU62D_0g_grasp_1.npy")
 data = np.load(numpy_file_path)
 plt.plot(qualisys_data.time_axis, data)
 plt.grid(linestyle=':', linewidth=1.5)
-plt.title('Front Shoulder Torque')
+plt.title('Elbow Torque')
 plt.xlabel('time (sec.)')
 plt.ylabel('Joint Torque (N-m)')
 plt.minorticks_on()
