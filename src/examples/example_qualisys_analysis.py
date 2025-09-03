d1c446e6c4ce3beb14c5c4481ded7ca021f15266
kschari <kartik.chari@dfki.de>
2025-09-03 16:46:47 +0200

    🐛 Works for shoulder_r_x and s_r_x

--- src/examples/example_qualisys_analysis.py
+++ src/examples/example_qualisys_analysis.py
@@ -12,11 +12,16 @@ qualisys_data.calculateTorque(body_weight_kg=80,
                               subject_biological_sex="male",
                               subject_hand_length_mm=113,
                               method='com')
+qualisys_data.calculateTorque(body_weight_kg=80, 
+                              obj_weight_g=0, 
+                              subject_biological_sex="male",
+                              subject_hand_length_mm=113,
+                              method='com')
 #? save the torques into individual .npy files
 qualisys_data.saveTorques_npy(save_torques=True, save_dir="results/BU62D", joint_to_save=['all'])
 
 #? plot front shoulder torque profile
-numpy_file_path = getAbsolutePath("results/BU62D/quali_torque_elbow_24_07_2025_BU62D_0g_grasp_1.npy")
+numpy_file_path = getAbsolutePath("data/test/quali_torque_shoulder_side_complex_0g_set6.npy")
 data = np.load(numpy_file_path)
 plt.plot(qualisys_data.time_axis, data)
 plt.grid(linestyle=':', linewidth=1.5)
