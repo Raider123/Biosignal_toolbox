fea64a1a57e12a08053b022de65d16c3473d8f04
kschari <kartik.chari@dfki.de>
2025-09-04 15:01:24 +0200

    📝 Added doc and made some util functions robust

--- lib/biosignal_toolbox/utils.py
+++ lib/biosignal_toolbox/utils.py
@@ -4,11 +4,13 @@
 #! ************************************************
 
 import numpy as np
+import yaml
 import matplotlib.pyplot as plt
 from pathlib import Path
 from yaml import safe_load
 from datetime import datetime
 from types import SimpleNamespace
+from typing import Type, Union, List
 from sys import exit, path
 path.append(str(Path(__file__).resolve().parents[2]))
 from config_root import project_root
@@ -18,7 +20,7 @@ import inspect
 #! Custom Warning function
 #! ************************************************
 
-def customWarningFormat(message, category, filename, lineno, line=None):
+def customWarningFormat(message: str, category: Type[Warning], filename: str, lineno: int, line: str=None) -> str:
     """
     Format warnings in a compact way by including only the category, 
     function name, and message.
@@ -65,7 +67,7 @@ warnings.formatwarning = customWarningFormat
 #! Utility Functions
 #! ************************************************
 
-def loadConfig(filename=''):
+def loadConfig(filename: str = None) -> dict:
     """
     This function safely loads the yaml configuration file into a dictionary.
     It also converts the dict to a Namespace object wherein keys become attributes and can be accessed by '.' operator.
@@ -73,24 +75,58 @@ def loadConfig(filename=''):
     Parameters
     ----------
     filename : str
-        name of the config file
+        Name of the config file
+    
+    Returns
+    -------
+    dict
+        Dictionary containing all config parameters from the yaml file
+    
+    Raises
+    ------
+    IsADirectoryError
+        Raised if the filename is a dir and not a file
+    TypeError
+        Raised if the filename is None or not provided
+
     
     Author
     ------
-    Author: Kartik Chari \n
-    Last changed: 21.08.2025 (by Kartik Chari)
+    Author : Kartik Chari \n
+    Last changed : 21.08.2025 (by Kartik Chari)
     """
     try:
         project_root = getProjectRoot()
-        config_dir = project_root / 'config'
-        with open(config_dir / filename, 'r') as file:
+        # prepend 'config/' only if filename does not consist of this
+        if Path(filename).parts[0] != 'config':
+            filename = 'config/' + filename
+
+        with open(project_root / filename, 'r') as file:
             dict_obj = safe_load(file)
         return convertDictToNamespace(dict_obj)
-    except IsADirectoryError:
-        print("ERROR: Please enter correct config filename!!")
+    except (IsADirectoryError, TypeError):
+        print(f"ERROR: Please enter correct config filename in loadConfig!!")
         exit(1)
 
-def convertDictToNamespace(data_inp):
+def convertDictToNamespace(data_inp: Union[dict, list]) -> SimpleNamespace:
+    """
+    This function converts the input into a namespace that can be accessed like an attribute using a '.' operator.
+
+    Parameters
+    ----------
+    data_inp : Union[dict, list]
+        Input dict or list
+
+    Returns
+    -------
+    SimpleNamespace
+        Namespace of dict/list elements
+    
+    Author
+    ------
+    Author : Kartik Chari \n
+    Last changed : 21.08.2025 (by Kartik Chari)
+    """
     if isinstance(data_inp, dict):
         return SimpleNamespace(**{k: convertDictToNamespace(v) for k, v in data_inp.items()})
     elif isinstance(data_inp, list):
@@ -99,22 +135,62 @@ def convertDictToNamespace(data_inp):
         return data_inp
 
 
-def getProjectRoot(marker=""):
+def convertNamespaceToDict(data_inp: SimpleNamespace) -> dict:
     """
-    This function returns the project root folder path
+    This function converts the input nested namespace/dict  into a dict.
+
+    Parameters
+    ----------
+    data_inp : SimpleNamespace
+        Input NameSpace object
+
+    Returns
+    -------
+    dict
+        Dictionary converted from SimpleNamespace
+    
+    Author
+    ------
+    Author : Kartik Chari \n
+    Last changed : 04.09.2025 (by Kartik Chari)
+    """
+    if isinstance(data_inp, SimpleNamespace):
+        return {k: convertNamespaceToDict(v) for k, v in vars(data_inp).items()}
+    elif isinstance(data_inp, dict):
+        return {k: convertNamespaceToDict(v) for k, v in data_inp.items()}
+    elif isinstance(data_inp, (list, tuple)):
+        return [convertNamespaceToDict(v) for v in data_inp]
+    else:
+        return data_inp
+
+
+def getProjectRoot(marker: str = None) -> Path:
+    """
+    This function returns the project root folder path.
 
     Parameters
     -----
     marker: str, optional
-        The dir name that helps to looks for the root folder in any project structure, by default "".
+        The dir name that helps to looks for the root folder in any project structure, by default ""
+    
+    Returns
+    -------
+    Path
+        Project root filepath
+    
+    Raises
+    ------
+    RuntimeError
+        Raised if the marker is not found in the filepath stems
 
     Author
     ------
-    Author: Kartik Chari \n
-    Last changed: 28.08.2025 (by Kartik Chari)
+    Author : Kartik Chari \n
+    Last changed : 28.08.2025 (by Kartik Chari)
     """
     filepath = Path(__file__).resolve()
     if not marker:
+        print("No markers provided! Assuming that there are not further sub-divisions in your folder structure!!")
         return filepath.parent.parent.parent
     else:
         for parent in filepath.parents:
@@ -123,21 +199,34 @@ def getProjectRoot(marker=""):
         raise RuntimeError(f"Project root not found. Looked for {marker}")
 
 
-def getAbsolutePath(input_path=''):
+def getAbsolutePath(input_path: str = None) -> Path:
     """
     This function ensures that the input path is absolute. It first gets the root of the project and then appends the input path to it. This function does not work with relative paths.
 
     Parameters
     -----
     input_path: str
-        Path (wrt project root) to be made absolute, by default empty.
+        Path (wrt project root) to be made absolute, by default empty
+    
+    Returns
+    -------
+    Path
+        Absolute path of the input_path
+    
+    Returns
+    -------
+    ValueError
+        Raised if the input_path is None
+    ValueError
+        Raised if project root is not generated properly
+
     Author
     ------
-    Author: Kartik Chari \n
-    Last changed: 27.08.2025 (by Kartik Chari)
+    Author : Kartik Chari \n
+    Last changed : 27.08.2025 (by Kartik Chari)
     """
-    if not input_path:
-        raise ValueError("Please provide input path and/or project root path!")
+    if input_path is None:
+        raise ValueError("Please provide a valid input path !!")
     # get project root
     project_root = getProjectRoot()
 
@@ -155,7 +244,7 @@ def getAbsolutePath(input_path=''):
     return (project_root / relative_part).resolve()
 
 
-def createOutputDir(param_obj=None, suffix_str=''):
+def createOutputDir(param_obj: SimpleNamespace = None, suffix_str: str = None) -> Path:
     """
     This function checks whether the fig_save_path exists or not. If it does not exist, it will create the directory. In addition, it will create the output dir where the output plots and readme will be saved.
     The output dir will look like fig_save_path/sub_code/date_time_plot_1. It also checks the last integer and increments it to prevent overwrite.
@@ -163,24 +252,31 @@ def createOutputDir(param_obj=None, suffix_str=''):
     Parameters
     ----------
     param_obj: dict
-        param dict imported from the .yaml file, by default None.
+        param dict imported from the .yaml file, by default None
     suffix_str: str
-        suffix for the output dir, by default "".
+        suffix for the output dir, by default ""
         
     Returns
     -----
     Path
-        path of the output directory.
+        path of the output directory
+    
+    Raises
+    ------
+    RuntimeError
+        Raised if param_obj is None
     
     Author
     ------
-    Author: Kartik Chari \n
-    Last changed: 13.11.2025 (by Kartik Chari)
+    Author : Kartik Chari \n
+    Last changed : 13.11.2024 (by Kartik Chari)
     """
+    #TODO: Make it general
     if param_obj is None:
         raise RuntimeError("Please provide the yaml config object!!")
-    if not suffix_str:
-        suffix_str = "plot"
+    if suffix_str is None:
+        warnings.warn(f"Suffix_str is {suffix_str}. Using \"temp\" instead!!")
+        suffix_str = "temp"
     
     inp_parent_dir = getAbsolutePath(param_obj.filepath.fig_save_path)
     parent_dir = inp_parent_dir / f"{param_obj.data_param.subject_code}"
@@ -203,57 +299,70 @@ def createOutputDir(param_obj=None, suffix_str=''):
     return new_dir
 
 
-def createReadme(param_obj=None, dir_path=None):
+def createReadme(param_obj: Union[SimpleNamespace, dict] = None, dir_path: Path = None, sections_to_include: List[str] = None) -> None:
     """
     This function creates a readme file in the same dir as the plots with all necessary hyperparameters of the model.
 
     Parameters
     ----------
-    param_obj : dict
-        param dict imported from the .yaml file, by default None
-    dir_path: pathlib Path obj
-        path for the readme file
+    param_obj : dict or SimpleNamespace
+        Object imported from the .yaml file, by default None
+    dir_path: Path
+        Path of dir where readme file needs to be created
+    sections_to_include: list of str
+        Names of the sections that need to be included in the readme
+    
+    Raises
+    ------
+    TypeError
+        Raised if param_obj is neither a dict nor SimpleNamespace
+    KeyError
+        Raised if the dict keys filepath or fig_save_path do not exist
     
     Author
     ------
-    Author: Kartik Chari \n
-    Last changed: 13.11.2025 (by Kartik Chari)
+    Author : Kartik Chari \n
+    Last changed : 04.09.2025 (by Kartik Chari)
     """
-    if dir_path is None:
-        print("WARNING: No dir_path specified! Creating readme file in the fig_save_path...")
-        dir_path = Path(param_obj.filepath.fig_save_path)
-    
     try:
-        readme_file = dir_path / "readme.txt"
-        with readme_file.open("w") as f:
-            f.write(f"Scenario: {param_obj.data_param.weights, param_obj.data_param.mov_type}\n") 
-            f.write(f"Subject Code: {param_obj.data_param.subject_code}\n")
-            f.write(f"Batch Size: {param_obj.model_param.batch_size}\n")
-            f.write(f"Epochs: {param_obj.model_param.n_epochs}")
-            f.write(f"Feature Selection: {param_obj.preprocess_param.feature_select}\n")
-            f.write(f"Window Size X: {param_obj.preprocess_param.window_size_x}\n")
-            f.write(f"Window Size Y: {param_obj.preprocess_param.window_size_y}\n")
-            f.write(f"Window Step: {param_obj.preprocess_param.window_step}\n")
-            f.write(f"\n")
-            f.write(f"HPF Filter: {param_obj.preprocess_param.f_cutoff_hpf}Hz\n")
-            f.write(f"Variance Filter width: {param_obj.preprocess_param.var_filter_width}\n")
-            f.write(f"MVC: {param_obj.preprocess_param.mvc}\n")
-            f.write(f"LPF Filter: {param_obj.preprocess_param.f_cutoff_lpf}Hz\n")
-            f.write(f"Force Activation: {param_obj.preprocess_param.act_delay, param_obj.preprocess_param.act_beta1, param_obj.preprocess_param.act_beta2, param_obj.preprocess_param.act_gamma, param_obj.preprocess_param.act_A}\n")
-            f.write(f"\n")
-            f.write(f"BPNN Neurons: {param_obj.model_param.neurons_h1, param_obj.model_param.neurons_h2}\n")
-            f.write(f"BPNN Act functions: {param_obj.model_param.act_inp, param_obj.model_param.act_h1, param_obj.model_param.act_h2}\n")
-            f.write(f"Early Stopping: {param_obj.model_param.is_early_stop}\n")
-            f.write(f"Post Processing Filter type: {param_obj.post_train_param.filter_type}\n")
-            f.write(f"Post Processing Filter size: {param_obj.post_train_param.filter_size}\n")
+        # convert SimpleNamespace → dict only if needed
+        if isinstance(param_obj, SimpleNamespace):
+            param_dict = convertNamespaceToDict(param_obj)
+        elif isinstance(param_obj, dict):
+            param_dict = convertNamespaceToDict(param_obj)
+        else:
+            raise TypeError("param_obj must be a dict or SimpleNamespace!!")
+
+        if dir_path is None:
+            warnings.warn("Missing dir_path argument!! Trying to access fig_save_path from config instead!!")
+            if "filepath" not in param_dict or "fig_save_path" not in param_dict["filepath"]:
+                raise KeyError("Missing filepath.fig_save_path in config file!!!")
+            dir_path = getAbsolutePath(param_dict["filepath"]["fig_save_path"])
     
+        readme_file = dir_path / "readme.txt"
+        readme_file.parent.mkdir(parents=True, exist_ok=True)
+
+        # filter sections if needed
+        if sections_to_include is not None:
+            filter_dict = {}
+            for sec in sections_to_include:
+                if sec in param_dict:
+                    filter_dict[sec] = param_dict[sec]
+                else:
+                    filter_dict[sec] = "(NOT FOUND)"
+        else:
+            filter_dict = param_dict
+        
+        with readme_file.open("w") as f:
+            f.write("Configuration Used for Training!!\n\n")
+            yaml.dump(filter_dict, f, sort_keys=False, default_flow_style=False)
+        
+        print(f"Readme created at: {readme_file}")
+
     except KeyError:
-        print("ERROR!! Please ensure you pass non-empty dict object!")
+        print("ERROR!! Please ensure you pass correct args to createReadme!!")
         exit(1)
     
-    finally:
-        f.close()
-
 
 def plotResults(data_ref=[], label_ref="real", data_out=[], label_out="predicted", title="", xlabel="Time in s", ylabel="", is_grid_on=True):
     """
@@ -280,9 +389,10 @@ def plotResults(data_ref=[], label_ref="real", data_out=[], label_out="predicted
     
     Author
     ------
-    Author: Kartik Chari \n
-    Last changed: 13.11.2025 (by Kartik Chari)
+    Author : Kartik Chari \n
+    Last changed : 13.11.2024 (by Kartik Chari)
     """
+    #TODO: Improve to make it more general
     plt.figure()
 
     x_samples = np.arange(0, len(data_ref),1)
