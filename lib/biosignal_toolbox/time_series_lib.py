Fixed variance filter to only consider values not previously filtered

lib/biosignal_toolbox/time_series_lib.py
@@ -944,6 +944,7 @@ class Timeseries():
             self.data_buffer[0, ch, -self.n_samples:, 0] = y_block
 
 
+
     def filterWindows(self, b = None, a = [1], sos = None, apply_method = "zero_phase_sos", mne_filter_type = None, f_high = None, f_low = None, order = None, fir_design = None, padtype = "even"): # under change 
         """
         Apply a designed digital filter to the windowed data (window wise for each channel). Please be careful in selection appropriately designed filters, especially because they are applied on small data chunks (windows)!
@@ -2826,7 +2827,7 @@ class Timeseries():
                 self.data_buffer[0,ch,(-1*self.n_samples):,0], zi_out = sosfilt(sos,self.data_buffer[0,ch,(-1*self.n_samples):,0], zi=np.expand_dims(self.zi_hpf[ch,:], axis=0))
                 self.zi_hpf[ch,:] = zi_out
 
-    def applyVarianceFilter_data(self, ring_buffer=None, width=20, index=0, mode="offline"):
+    def applyVarianceFilter_data(self, ring_buffer=None, width=20, index=0, mode="offline", var_buffer=np.zeros((1,8,100,1))):
         """
         This method applies variance filter on the complete data using the cpp variance_tools API
 
@@ -2854,15 +2855,18 @@ class Timeseries():
 
         elif mode == "old_online":
             out = self.data_buffer[0, :, :, 0]  # View: (C, N)
+            old_buffer = var_buffer[0, :, :, 0]
             C, N = out.shape
+
             start = N - self.n_samples  # wir bearbeiten nur [start : N)
 
             # kumulative Summen (für Var ohne Schleifen)
             # Padding links mit 0, damit Fenster [i-n_var:i) = cs[i] - cs[i-n_var]
-            cs = np.concatenate([np.zeros((C, 1), dtype=np.float64),
-                                 np.cumsum(out, axis=1, dtype=np.float64)], axis=1)
-            cs2 = np.concatenate([np.zeros((C, 1), dtype=np.float64),
-                                  np.cumsum(out * out, axis=1, dtype=np.float64)], axis=1)
+            old_cs = np.concatenate([np.zeros((C, 1), dtype=np.float64),
+                                 np.cumsum(old_buffer, axis=1, dtype=np.float64)], axis=1)
+            old_cs2 = np.concatenate([np.zeros((C, 1), dtype=np.float64),
+                                  np.cumsum(old_buffer * old_buffer, axis=1, dtype=np.float64)], axis=1)
+
 
             # Teil 1: im Segment [start : min(n_var, N)) -> 0 (wie im raw-Code)
             i0 = start
@@ -2876,12 +2880,14 @@ class Timeseries():
                 # Fenster-Summen für jedes i in [j0 : N):
                 # Sum = cs[:, i] - cs[:, i - n_var]
                 # Sum2 = cs2[:, i] - cs2[:, i - n_var]
-                sum_w = cs[:, j0:N] - cs[:, j0 - width: N - width]
-                sum2_w = cs2[:, j0:N] - cs2[:, j0 - width: N - width]
+                sum_w = old_cs[:, j0:N] - old_cs[:, j0 - width: N - width]
+                sum2_w = old_cs2[:, j0:N] - old_cs2[:, j0 - width: N - width]
                 denom = float(width)
                 var_seg = (sum2_w / denom) - (sum_w / denom) ** 2
                 out[:, j0:N] = var_seg.astype(out.dtype)
 
+
+            self.variance_buffer = self.data_buffer[0, :, :, 0]
 
     def normalizeContinuousData(self, mvc=0, mode="offline"):
         """

src/JTE_Project/online/emg_receiver.py
@@ -68,6 +68,8 @@ class LiveEstimation:
                                  f_samp=self.property["f_samp"])
        print("Created EMG_live!!")
 
+       self.var_buffer = np.zeros((1, len(self.channel_names), self.property["buffer_size"], 1))
+
        # Design bandpass filter
        self.sos_bp = self.EMG_live.designFilter(f_high=self.cfg.preprocess_param.f_cutoff_hpf,
                                                 f_low=self.cfg.preprocess_param.f_cutoff_lpf,
@@ -403,7 +405,6 @@ class LiveEstimation:
    def save_emg_vals(self):
        all_emgs = np.concatenate(self.all_emg_vals, axis=1)
        np.save(getAbsolutePath("src/JTE_Project/offline/filter_tests/emg.npy"), all_emgs)
-
        print("Save EMG Shape: ", all_emgs.shape)
 
 
@@ -425,11 +426,18 @@ class LiveEstimation:
            # * apply bandpass filter (previous highpass filter)
            self.EMG_live.filterBuffer(sos=self.sos_bp)
 
-           # Create identical copy for frequency extraction
-           self.EMG_live_freq = copy.deepcopy(self.EMG_live)
+           self.var_buffer = np.roll(self.var_buffer, shift = int(-1*self.batch_size), axis = 2)
+           self.var_buffer[0, :, int(-1*self.batch_size):, 0] = self.EMG_live.getDataBuffer()[0, :, int(-1*self.batch_size):, 0]
 
+           # Create identical copy for frequency extraction 
+           
+           self.EMG_live_freq = copy.deepcopy(self.EMG_live)
+           
            # # variance filter
-           self.EMG_live.applyVarianceFilter_data(width=20, mode="old_online")
+           
+           self.EMG_live.applyVarianceFilter_data(width=20, mode="old_online", var_buffer=self.var_buffer)
+
+
 
            # # normalisation #
            self.EMG_live.normalizeContinuousData(mvc=self.property["mvc"], mode = "old_online")
@@ -441,7 +449,7 @@ class LiveEstimation:
 
            # convert filtered data into window
            self.EMG_live.bufferToWindows()
-           self.EMG_live_freq.bufferToWindows()
+           #self.EMG_live_freq.bufferToWindows()
 
            update_time_step = time.time() - update_start_time
            self.current_time = self.current_time + update_time_step

src/JTE_Project/online/highpass_offline_test_plot.py
@@ -30,8 +30,9 @@ plt.show()
 variance_raw_offline = np.load(getAbsolutePath("src/JTE_Project/offline/filter_tests/variance_offline.npy"))
 variance_raw_online = np.load(getAbsolutePath("src/JTE_Project/offline/filter_tests/variance_online.npy"))
 
-variance_online = variance_raw_online[4,:]
-variance_offline = variance_raw_offline[4,:variance_raw_online.shape[1]]
+variance_offline = variance_raw_offline[4,:]
+variance_online = variance_raw_online[4,:variance_raw_offline.shape[1]]
+
 
 print(variance_offline.shape, " ", variance_online.shape)
 
@@ -54,19 +55,20 @@ plt.show()
 mvc_raw_offline = np.load(getAbsolutePath("src/JTE_Project/offline/filter_tests/normalization_offline.npy"))
 mvc_raw_online = np.load(getAbsolutePath("src/JTE_Project/offline/filter_tests/normalization_online.npy"))
 
-mvc_online = mvc_raw_online[4,:]
-mvc_offline = mvc_raw_offline[4,:mvc_raw_online.shape[1]]
+mvc_offline = mvc_raw_offline[4,:]
+mvc_online = mvc_raw_online[4,:mvc_raw_offline.shape[1]]
+
 
 
 print(mvc_offline.shape, " ", mvc_online.shape)
 
-x = np.linspace(0,1, mvc_online.shape[0])
+x = np.linspace(0,1, mvc_offline.shape[0])
 y5 = mvc_online
 y6 = mvc_offline
 # Plot erstellen
 plt.figure(figsize=(12, 6))
 
-#plt.plot(x, y5, label='MVC Online', linestyle='-')
+plt.plot(x, y5, label='MVC Online', linestyle='-')
 plt.plot(x, y6, label='MVC Offline', linestyle='-')
 
 # Plot konfigurieren